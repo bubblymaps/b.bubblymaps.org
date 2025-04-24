caf2d56fd50520ac000be4aeb9c133d4f8e4a8f8
Linus Kang <linus@kang.id.au>
2025-04-25 09:47:23 +1000

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const redirects = require("./redirects.json");
+
+for (const slug in redirects) {
+  const url = redirects[slug];
+  const dir = path.join(__dirname, slug);
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta http-equiv="refresh" content="0; url=${url}">
+        <link rel="canonical" href="${url}">
+      </head>
+    </html>`;
+
+  fs.mkdirSync(dir, { recursive: true });
+  fs.writeFileSync(path.join(dir, "index.html"), html.trim());
+}
