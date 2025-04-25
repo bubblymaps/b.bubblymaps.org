1ba7dd9d34b067c2399f53fd711bff3f2be93f04
Linus Kang <linus@kang.id.au>
2025-04-25 10:32:15 +1000

    made so that it can delete old paths

--- generate.js
+++ generate.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const redirects = require("./redirects.json");
 
+const existingDirs = fs.readdirSync(__dirname).filter(file => fs.statSync(path.join(__dirname, file)).isDirectory());
+
 for (const slug in redirects) {
   const url = redirects[slug];
   const dir = path.join(__dirname, slug);
@@ -18,3 +20,11 @@ for (const slug in redirects) {
   fs.mkdirSync(dir, { recursive: true });
   fs.writeFileSync(path.join(dir, "index.html"), html.trim());
 }
+
+existingDirs.forEach((dir) => {
+  if (!redirects[dir]) {
+    const dirPath = path.join(__dirname, dir);
+    fs.rmdirSync(dirPath, { recursive: true });
+    console.log(`Deleted old redirect: ${dir}`);
+  }
+});
